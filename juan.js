a665bb808f57c3033604cb6243daf0ee1441222f
Juan Del Rio <52029674+juandrh@users.noreply.github.com>
2022-09-11 17:42:36 +0200

    Add files via upload

--- juan.js
+++ juan.js
@@ -6,6 +6,10 @@
 
 import * as THREE from 'three';
 import { GLTFLoader } from 'GLTFLoader';
+import { FirstPersonControls} from 'Controls';
+
+
+
 
 function main() {
   const canvas = document.querySelector('#c');
@@ -13,10 +17,12 @@ function main() {
 
   const fov = 75;
   const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 5;
+  const near = 0.001;
+  const far = 50000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.z = 2;
+  let FPcontrols;
+  const clock = new THREE.Clock();
 
   const scene = new THREE.Scene();
 
@@ -37,12 +43,26 @@ function main() {
   });
 
 
-  const loader2 = new GLTFLoader();
+  /* const loader2 = new GLTFLoader();
     loader2.load( './assets/scene.gltf', function ( gltf ) {    
       scene.add( gltf.scene );    
     }, undefined, function ( error ) {    
       console.error( error );    
-    } );
+    } ); */
+
+
+   const loader2 = new GLTFLoader();
+
+    loader2.load( './assets/untitled.glb', function ( gltf ) {    
+      scene.add( gltf.scene );    
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+    ); 
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -55,6 +75,15 @@ function main() {
     return needResize;
   }
 
+  FPcontrols = new FirstPersonControls(camera, canvas);
+  FPcontrols.movementSpeed = 1.50;
+  FPcontrols.lookSpeed = 0.1;
+
+  const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+ 
   function render(time) {
     time *= 0.001;
 
@@ -73,8 +102,8 @@ function main() {
 
 
     
-
-
+    FPcontrols.update( clock.getDelta() );
+    
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
