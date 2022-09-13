a255ef02bdfdb44f0e7b77c8b4b1ef2467a0f898
Juan Del Rio <52029674+juandrh@users.noreply.github.com>
2022-09-13 22:04:35 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 
 import * as THREE from 'three';
 import { PointerLockControls } from 'Controls';
-import Stats from 'stats'; //"/jsm/libs/stats.module.js";
+import Stats from 'stats'; 
+import { GLTFLoader } from 'GLTFLoader';
 
 
 
@@ -19,6 +20,8 @@ let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
 let canJump = false;
+let cameraY= 15;
+//let floorSize = 10;
 
 let prevTime = performance.now();
 const velocity = new THREE.Vector3();
@@ -35,10 +38,12 @@ const fftSize = 512;
 var dataArray = new Uint8Array(fftSize);
 const stats = Stats();
 
-
-const boxWidth = 4;
+const loader = new GLTFLoader();
+const boxWidth = 0.5;
 let cube = new THREE.Mesh(); 
-const cubes = [];    
+let cube2 = new THREE.Mesh(); 
+const cubes = []; 
+const cubes2 = [];    
 const materials = [];  
 
 init();
@@ -49,16 +54,23 @@ function init() {
 
   document.body.appendChild(stats.dom);
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.y = 10;
+  camera.position.y = cameraY;
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xffffff);
-  scene.fog = new THREE.Fog(0xffffff, 0, 750);
+  scene.background = new THREE.Color(0x0000ff);
+  scene.fog = new THREE.Fog(0x0000ff, 0, 200);
 
   // LIGHTS
   const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
   light.position.set(0.5, 1, 0.75);
-  scene.add(light);
+  //scene.add(light);
+
+  
+  const light2 = new THREE.DirectionalLight(0xFFFFFF, 1);
+  light2.position.set(50, 100, 0);
+  light2.target.position.set(-50, 0, 0);
+  scene.add(light2);
+
 
   controls = new PointerLockControls(camera, document.body);
 
@@ -140,70 +152,86 @@ function init() {
   raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10);
 
   // floor
-  let floorGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
+  let floorGeometry = new THREE.PlaneGeometry(2000, 2000, 400, 400);
   floorGeometry.rotateX(- Math.PI / 2);
+    
+    let position = floorGeometry.attributes.position;
 
-  // vertex displacement
-  let position = floorGeometry.attributes.position;
-
-  for (let i = 0, l = position.count; i < l; i++) {
-    vertex.fromBufferAttribute(position, i);
-    vertex.x += 10;     
-    vertex.z += 10; 
-    position.setXYZ(i, vertex.x, vertex.y, vertex.z);
-  }
-
-  floorGeometry = floorGeometry.toNonIndexed(); 
-  position = floorGeometry.attributes.position;
-  const colorsFloor = [];
-
-  // Floor color
-  for (let i = 0, l = position.count/6; i < l; i++) {    
-    let rand = Math.random();
-    color.setRGB(rand * 0.3 + 0.5, rand *  0.3 + 0.5,rand *  0.1 + 0.0); 
-    for (let j = 0 ; j < 6; j++) {
-    colorsFloor.push(color.r, color.g, color.b);
+    for (let i = 0, l = position.count; i < l; i++) {
+      vertex.fromBufferAttribute(position, i);
+      vertex.x += 10;     
+      vertex.z += 10; 
+      position.setXYZ(i, vertex.x, vertex.y, vertex.z);
     }
-  }
+  
+    floorGeometry = floorGeometry.toNonIndexed(); 
+    position = floorGeometry.attributes.position;
+    const colorsFloor = [];
+  
+    // Floor color
+    for (let i = 0, l = position.count/6; i < l; i++) {    
+      let rand = Math.random();
+      color.setRGB(rand * 0.02 + 0.05, rand *  0.01 + 0.005,rand *  0.005 + 0.0); 
+      for (let j = 0 ; j < 6; j++) {
+      colorsFloor.push(color.r, color.g, color.b);
+      }
+    }
+  
+    floorGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsFloor, 3));
+  
+    const floorMaterial = new THREE.MeshStandardMaterial({ vertexColors: true });
+  
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    scene.add(floor);
 
-  floorGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsFloor, 3));
+  // house 1
+  loader.load( './assets/house.glb', function ( gltf ) {  
+    gltf.scene.position.z=-200; 
+    gltf.scene.position.x=-120; 
+    scene.add( gltf.scene );    
+  },
+  (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+  },
+  (error) => {console.log(error) }
+  ); 
 
-  const floorMaterial = new THREE.MeshStandardMaterial({ vertexColors: true });
+   // house 2
+   loader.load( './assets/house.glb', function ( gltf ) {  
+    gltf.scene.position.z=-200; 
+    gltf.scene.position.x=120; 
+    scene.add( gltf.scene );    
+  },
+  (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+  },
+  (error) => {console.log(error) }
+  ); 
 
-  const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  scene.add(floor);
+   // house 3
+   loader.load( './assets/house.glb', function ( gltf ) {  
+    gltf.scene.position.z=-50; 
+    gltf.scene.position.x=120; 
+    scene.add( gltf.scene );    
+  },
+  (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+  },
+  (error) => {console.log(error) }
+  ); 
 
-  // objects
+  // house 4
+  loader.load( './assets/house.glb', function ( gltf ) {  
+    gltf.scene.position.z=-50; 
+    gltf.scene.position.x=-120; 
+    scene.add( gltf.scene );    
+  },
+  (xhr) => {
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+  },
+  (error) => {console.log(error) }
+  ); 
 
-  /* const boxGeometry = new THREE.BoxGeometry(20, 20, 20).toNonIndexed();
-
-  position = boxGeometry.attributes.position;
-  const colorsBox = [];
-
-  for (let i = 0, l = position.count; i < l; i++) {
-
-    color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-    colorsBox.push(color.r, color.g, color.b);
-
-  }
-
-  boxGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsBox, 3));
-
-  for (let i = 0; i < 500; i++) {
-
-    const boxMaterial = new THREE.MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: true });
-    boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-
-    const box = new THREE.Mesh(boxGeometry, boxMaterial);
-    box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
-    box.position.y = Math.floor(Math.random() * 20) * 20 + 10;
-    box.position.z = Math.floor(Math.random() * 20 - 10) * 20;
-
-    scene.add(box);
-    objects.push(box);
-
-  }
- */
   //  BOXES
 
  
@@ -211,7 +239,7 @@ function init() {
 
 
 
-  var step=100;
+  var step=25;
   for (var i=0;i<fftSize/2 ; i++){
       const material = new THREE.MeshPhysicalMaterial(); 
       material.color.setRGB(0,155,0);  
@@ -219,15 +247,21 @@ function init() {
       material.shininess = 0.1; 
       material.roughness = 0.9;
       material.metalness = 0.0;
-      material.fog = true;
+      //material.fog = true;
       cube = new THREE.Mesh(geometry, material);  
+      cube2 = new THREE.Mesh(geometry, material);  
       scene.add(cube);
+      scene.add(cube2);
       //cube.position.x = i*boxWidth*2 - (boxWidth*fftSize/2);
 
-      cube.position.x = step*Math.cos(i*Math.PI*4/fftSize);
+      cube.position.x = step-step*Math.cos(i*Math.PI*2/fftSize);
       cube.position.y = -1;
-      cube.position.z = step*Math.sin(i*Math.PI*4/fftSize);
+      cube.position.z = -step*Math.sin(i*Math.PI*2/fftSize)-120;
       cubes.push(cube); 
+      cube2.position.x = -step+step*Math.cos(i*Math.PI*2/fftSize);
+      cube2.position.y = -1;
+      cube2.position.z = -step*Math.sin(i*Math.PI*2/fftSize)-120;
+      cubes2.push(cube2);        
       materials.push(material);   
   }
 
@@ -264,7 +298,8 @@ function initMusic() {
     const mloader = new THREE.AudioLoader();
     mloader.load( file, function ( buffer ) {
       audio.setBuffer( buffer );
-      audio.setLoop( true );
+      audio.setLoop( true );      
+	    audio.setVolume( 1.0 );
       audio.play();
       
     } );
@@ -273,7 +308,8 @@ function initMusic() {
     
     mediaElement.play();
     audio.setMediaElementSource( mediaElement );
-    audio.setLoop( true );
+    audio.setLoop( true );         
+    audio.setVolume( 1.0 );
   }
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();	
   analyser = new THREE.AudioAnalyser( audio, fftSize );			
@@ -294,7 +330,7 @@ function animate() {
   if (controls.isLocked === true) {
 
     raycaster.ray.origin.copy(controls.getObject().position);
-    raycaster.ray.origin.y -= 10;
+    raycaster.ray.origin.y -= cameraY;
 
     const intersections = raycaster.intersectObjects(objects, false);
 
@@ -326,10 +362,10 @@ function animate() {
 
     controls.getObject().position.y += (velocity.y * delta); // new behavior
 
-    if (controls.getObject().position.y < 10) {
+    if (controls.getObject().position.y < cameraY) {
 
       velocity.y = 0;
-      controls.getObject().position.y = 10;
+      controls.getObject().position.y = cameraY;
 
       canJump = true;
 
@@ -339,6 +375,7 @@ function animate() {
   // -----------------------------------------------------------
   for (var i=0;i<fftSize/2 ; i++){ 
     cube = cubes[i];
+    cube2 = cubes2[i];
     const speed = .2 + i * .1;
     const rot = time * speed;
   //  cube.rotation.x = rot;
@@ -350,12 +387,12 @@ function animate() {
       dataArray = analyser.getFrequencyData();  
      
       //cube = new THREE.Mesh(geometry, material);
-      cube.scale.y = dataArray[i]/16; 
-      console.log(dataArray[i]/128);
-      cube.position.y = dataArray[i]/256+1;
+      cube.scale.y = dataArray[i]/2;       
+      cube.position.y = dataArray[i]/64;
       cube.material.color.setRGB( 50,dataArray[i]/256,1-dataArray[i]/256);
-      //color = new THREE.Color(0xffffff ); //0.25,dataArray[i]/256,dataArray[i]/256);
-      //cube.material.set(material);
+      cube2.scale.y = dataArray[i]/2;       
+      cube2.position.y = dataArray[i]/64;
+      cube2.material.color.setRGB( 50,dataArray[i]/256,1-dataArray[i]/256);
       
       
       }
